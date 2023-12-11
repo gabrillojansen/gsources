0525a5d1d01862b9ca65347db18a3413d1a186a4
gabrillojansen <Gabrillojansen@gmail.com>
2023-12-11 19:57:10 +0800

    update changes

--- src/components/ArrowUp.jsx
+++ src/components/ArrowUp.jsx
@@ -15,7 +15,7 @@ export const ArrowUp = () => {
 
   return (
     <div
-    className={`fixed right-[1rem] lg:right-[3rem] flex items-center justify-center w-[30px] h-[30px] rounded-[5px] bg-[--arrow-up-bg] cursor-pointer hover:bottom-[3.2rem]
+    className={`fixed right-[1rem] lg:right-[3rem] flex items-center justify-center w-[30px] h-[30px] rounded-[5px] bg-[--arrow-up-bg] cursor-pointer
     ${showArrowUp ? "bottom-[3rem]" : "bottom-[-3rem]"}`}
     onClick={() => window.scrollTo(0, 0)}>
         <FaArrowUp
