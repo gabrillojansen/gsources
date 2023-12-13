8196b798ecf75cf891a34f6ad6f4cf264a313c64
gabrillojansen <Gabrillojansen@gmail.com>
2023-12-13 19:01:24 +0800

    commit

--- src/data/ITEMS.js
+++ src/data/ITEMS.js
@@ -1,305 +1,339 @@
-import gsourcesLogo from "../assets/gsources-logo.png"
-import htmlLogo from "../assets/html-logo.png"
-import cssLogo from "../assets/css-logo.png"
-import figmaLogo from "../assets/figma-logo.png"
-import drawioLogo from "../assets/dawio-logo.png"
-import zeplinLogo from "../assets/zeplin-logo.png"
-import googleFontsLogo from "../assets/googlefonts-logo.png"
-import fontinuseLogo from "../assets/fontinuse-logo.jpg"
-import fontfabricLogo from "../assets/fontfabric-logo.png"
-import khromaLogo from "../assets/khroma-logo.png"
-import happyhuesLogo from "../assets/happyhues-logo.avif"
-import feathericonsLogo from "../assets/feathericons-logo.png"
-import ioniconsLogo from "../assets/ionicon-logo.avif"
-import colorhuntLogo from "../assets/colorhunt-logo.png"
-import htmlColorCodesLogo from "../assets/logo-html-color-codes-logo.png"
-import coolorsLogo from "../assets/coolors-logo.png"
-import brandcolorsLogo from "../assets/brandcolors-logo.png"
-import blobmakerLogo from "../assets/blobmaker-logo.jpg"
-import softrLogo from "../assets/softr-logo.png"
-import uiverseioLogo from "../assets/uiverseio-logo.png"
-import atroposjsLogo from "../assets/atroposjs-logo.png"
-import animejsLogo from "../assets/animejs-logo.jpg"
-import gsapLogo from "../assets/gsap-logo.jpg"
-import html5upLogo from "../assets/html5up-logo.png"
-import webflowLogo from "../assets/webflow-logo.jpg"
-import magicpatternLogo from "../assets/magicpattern-logo.jpg"
-import l5Logo from "../assets/100l5-logo.png"
-import getwavesLogo from "../assets/getwaves-logo.png"
-import haikeiLogo from "../assets/haikei-logo.jpg"
-import shecodesLogo from "../assets/shecodes-logo.gif"
+import gsourcesLogo from '../assets/gsources-logo.png'
+import htmlLogo from '../assets/html-logo.png'
+import cssLogo from '../assets/css-logo.png'
+import figmaLogo from '../assets/figma-logo.png'
+import drawioLogo from '../assets/dawio-logo.png'
+import zeplinLogo from '../assets/zeplin-logo.png'
+import googleFontsLogo from '../assets/googlefonts-logo.png'
+import fontinuseLogo from '../assets/fontinuse-logo.jpg'
+import fontfabricLogo from '../assets/fontfabric-logo.png'
+import khromaLogo from '../assets/khroma-logo.png'
+import happyhuesLogo from '../assets/happyhues-logo.avif'
+import feathericonsLogo from '../assets/feathericons-logo.png'
+import ioniconsLogo from '../assets/ionicon-logo.avif'
+import colorhuntLogo from '../assets/colorhunt-logo.png'
+import htmlColorCodesLogo from '../assets/logo-html-color-codes-logo.png'
+import coolorsLogo from '../assets/coolors-logo.png'
+import brandcolorsLogo from '../assets/brandcolors-logo.png'
+import blobmakerLogo from '../assets/blobmaker-logo.jpg'
+import softrLogo from '../assets/softr-logo.png'
+import uiverseioLogo from '../assets/uiverseio-logo.png'
+import atroposjsLogo from '../assets/atroposjs-logo.png'
+import animejsLogo from '../assets/animejs-logo.jpg'
+import gsapLogo from '../assets/gsap-logo.jpg'
+import html5upLogo from '../assets/html5up-logo.png'
+import webflowLogo from '../assets/webflow-logo.jpg'
+import magicpatternLogo from '../assets/magicpattern-logo.jpg'
+import l5Logo from '../assets/100l5-logo.png'
+import getwavesLogo from '../assets/getwaves-logo.png'
+import haikeiLogo from '../assets/haikei-logo.jpg'
+import shecodesLogo from '../assets/shecodes-logo.gif'
+import scrollrevealLogo from '../assets/scrollreveal-logo.png'
+import emblacarouselLogo from '../assets/emblacarousel-logo.jpg'
 
 export const ITEMS = [
     {
         id: 1,
         image: htmlLogo,
-        title: "HTML",
-        description: "Documentation for HTML",
-        url: "https://devdocs.io/html/",
-        category: "documentations"
+        title: 'HTML',
+        description: 'Documentation for HTML',
+        url: 'https://devdocs.io/html/',
+        category: 'documentations'
     },
     {
         id: 2,
         image: cssLogo,
-        title: "CSS",
-        description: "Documentation for CSS",
-        url: "https://devdocs.io/css/",
-        category: "documentations"
+        title: 'CSS',
+        description: 'Documentation for CSS',
+        url: 'https://devdocs.io/css/',
+        category: 'documentations'
     },
     {
         id: 3,
         image: gsourcesLogo,
-        title: "Devdocs.io",
-        description: "DevDocs Combines Multiple API Documentations",
-        url: "https://devdocs.io/",
-        category: "documentations"
+        title: 'Devdocs.io',
+        description: 'DevDocs Combines Multiple API Documentations',
+        url: 'https://devdocs.io/',
+        category: 'documentations'
     },
     {
         id: 4,
         image: figmaLogo,
-        title: "Figma",
-        description: "Design and collaborate all in the browser",
-        url: "https://www.figma.com/",
-        category: "design-tools",
+        title: 'Figma',
+        description: 'Design and collaborate all in the browser',
+        url: 'https://www.figma.com/',
+        category: 'design-tools',
     },
     {
         id: 5,
         image: drawioLogo,
-        title: "Draw.io",
-        description: "Free online diagram software for making flowcharts",
-        url: "https://app.diagrams.net/",
-        category: "design-tools",
+        title: 'Draw.io',
+        description: 'Free online diagram software for making flowcharts',
+        url: 'https://app.diagrams.net/',
+        category: 'design-tools',
     },
     {
         id: 6,
         image: zeplinLogo,
-        title: "Zeplin",
-        description: "Handoff designs with specs, assets and code snippets",
-        url: "https://zeplin.io/",
-        category: "design-tools",
+        title: 'Zeplin',
+        description: 'Handoff designs with specs, assets and code snippets',
+        url: 'https://zeplin.io/',
+        category: 'design-tools',
     },
     {
         id: 7,
         image: gsourcesLogo,
-        title: "CSS Buttons",
-        description: "Click on your desired button and copy the HTML & CSS",
-        url: "https://cssbuttons.app/",
-        category: "frontend-tools",
+        title: 'CSS Buttons',
+        description: 'Click on your desired button and copy the HTML & CSS',
+        url: 'https://cssbuttons.app/',
+        category: 'frontend-tools',
     },
     {
         id: 8,
         image: uiverseioLogo,
-        title: "UIVERSE.IO",
-        description: "Library of free and customizable ui elements made with CSS or Tailwind",
-        url: "https://uiverse.io/",
-        category: "frontend-tools",
+        title: 'UIVERSE.IO',
+        description: 'Library of free and customizable ui elements made with CSS or Tailwind',
+        url: 'https://uiverse.io/',
+        category: 'frontend-tools',
     },
     {
         id: 9,
         image: gsourcesLogo,
-        title: "Transition css",
-        description: "Drop-in CSS transitions",
-        url: "https://www.transition.style/",
-        category: "frontend-tools",
+        title: 'Transition css',
+        description: 'Drop-in CSS transitions',
+        url: 'https://www.transition.style/',
+        category: 'frontend-tools',
     },
     {
         id: 10,
         image: atroposjsLogo,
-        title: "Atropos Js",
-        description: "JavaScript library to create stunning touch-friendly 3D parallax hover effects",
-        url: "https://atroposjs.com/",
-        category: "javascript-libraries",
+        title: 'Atropos Js',
+        description: 'JavaScript library to create stunning touch-friendly 3D parallax hover effects',
+        url: 'https://atroposjs.com/',
+        category: 'javascript-libraries',
     },
     {
         id: 11,
         image: animejsLogo,
-        title: "Anime Js",
-        description: "Anime.Js is a lightweight JavaScript animation library",
-        url: "https://animejs.com/",
-        category: "javascript-libraries",
+        title: 'Anime Js',
+        description: 'Anime.Js is a lightweight JavaScript animation library',
+        url: 'https://animejs.com/',
+        category: 'javascript-libraries',
     },
     {
         id: 12,
         image: gsapLogo,
-        title: "GSAP",
-        description: "A widly robust JavaScript animation library built for professionals",
-        url: "https://gsap.com/",
-        category: "javascript-libraries",
+        title: 'GSAP',
+        description: 'A widly robust JavaScript animation library built for professionals',
+        url: 'https://gsap.com/',
+        category: 'javascript-libraries',
     },
     {
         id: 13,
         image: googleFontsLogo,
-        title: "Google Fonts",
-        description: "Making the web more beautiful, fast, and open through great typography",
-        url: "https://fonts.google.com/",
-        category: "typography",
+        title: 'Google Fonts',
+        description: 'Making the web more beautiful, fast, and open through great typography',
+        url: 'https://fonts.google.com/',
+        category: 'typography',
     },
     {
         id: 14,
         image: fontinuseLogo,
-        title: "Font in Use",
-        description: "A searchable archive of typography design",
-        url: "https://fontsinuse.com/",
-        category: "typography",
+        title: 'Font in Use',
+        description: 'A searchable archive of typography design',
+        url: 'https://fontsinuse.com/',
+        category: 'typography',
     },
     {
         id: 15,
         image: fontfabricLogo,
-        title: "Font Fabric",
-        description: "High-quality typefaces, calligraphy and lettering",
-        url: "https://www.fontfabric.com/free-fonts/",
-        category: "typography",
+        title: 'Font Fabric',
+        description: 'High-quality typefaces, calligraphy and lettering',
+        url: 'https://www.fontfabric.com/free-fonts/',
+        category: 'typography',
     },
     {
         id: 16,
         image: colorhuntLogo,
-        title: "Color Hunt",
-        description: "Free and open platform for color inspiration",
-        url: "https://colorhunt.co/",
-        category: "colors",
+        title: 'Color Hunt',
+        description: 'Free and open platform for color inspiration',
+        url: 'https://colorhunt.co/',
+        category: 'colors',
     },
     {
         id: 17,
         image: khromaLogo,
-        title: "Khroma",
-        description: "Uses AI to learn which colors you like and creates palettes",
-        url: "https://www.khroma.co/",
-        category: "colors",
+        title: 'Khroma',
+        description: 'Uses AI to learn which colors you like and creates palettes',
+        url: 'https://www.khroma.co/',
+        category: 'colors',
     },
     {
         id: 18,
         image: happyhuesLogo,
-        title: "Happy Hues",
-        description: "Curated colors in context",
-        url: "https://www.happyhues.co/",
-        category: "colors",
+        title: 'Happy Hues',
+        description: 'Curated colors in context',
+        url: 'https://www.happyhues.co/',
+        category: 'colors',
     },
     {
         id: 19,
         image: gsourcesLogo,
-        title: "Streamline",
-        description: "Over 30,000 something icons",
-        url: "https://www.streamlinehq.com/",
-        category: "icons",
+        title: 'Streamline',
+        description: 'Over 30,000 something icons',
+        url: 'https://www.streamlinehq.com/',
+        category: 'icons',
     },
     {
         id: 20,
         image: feathericonsLogo,
-        title: "Feather Icons",
-        description: "Simply beautiful open source icons",
-        url: "https://feathericons.com/",
-        category: "icons",
+        title: 'Feather Icons',
+        description: 'Simply beautiful open source icons',
+        url: 'https://feathericons.com/',
+        category: 'icons',
     },
     {
         id: 21,
         image: ioniconsLogo,
-        title: "Ionicons",
-        description: "Loving hand-crafted. Open source icons",
-        url: "https://ionic.io/ionicons/",
-        category: "icons",
+        title: 'Ionicons',
+        description: 'Loving hand-crafted. Open source icons',
+        url: 'https://ionic.io/ionicons/',
+        category: 'icons',
     },
     {
         id: 22,
         image: html5upLogo,
-        title: "HTML5 UP",
-        description: "Free HTML5 templates",
-        url: "https://html5up.net/",
-        category: "html-templates",
+        title: 'HTML5 UP',
+        description: 'Free HTML5 templates',
+        url: 'https://html5up.net/',
+        category: 'html-templates',
     },
     {
         id: 23,
         image: webflowLogo,
-        title: "Webflow",
-        description: "Free website templates",
-        url: "https://webflow.com/free-website-templates",
-        category: "html-templates",
+        title: 'Webflow',
+        description: 'Free website templates',
+        url: 'https://webflow.com/free-website-templates',
+        category: 'html-templates',
     },
     {
         id: 24,
         image: coolorsLogo,
-        title: "Coolors",
-        description: "Fast color schemes generator",
-        url: "https://coolors.co/",
-        category: "colors",
+        title: 'Coolors',
+        description: 'Fast color schemes generator',
+        url: 'https://coolors.co/',
+        category: 'colors',
     },
     {
         id: 25,
         image: brandcolorsLogo,
-        title: "Brand Colors",
-        description: "Biggest collection of official brand color codes",
-        url: "https://brandcolors.net/",
-        category: "colors",
+        title: 'Brand Colors',
+        description: 'Biggest collection of official brand color codes',
+        url: 'https://brandcolors.net/',
+        category: 'colors',
     },
     {
         id: 26,
         image: htmlColorCodesLogo,
-        title: "HTML Color Codes",
-        description: "Get HTML color codes, Hex color codes, RGB and HSL values with our color picker",
-        url: "https://htmlcolorcodes.com/",
-        category: "colors",
+        title: 'HTML Color Codes',
+        description: 'Get HTML color codes, Hex color codes, RGB and HSL values with our color picker',
+        url: 'https://htmlcolorcodes.com/',
+        category: 'colors',
     },
     {
         id: 27,
         image: magicpatternLogo,
-        title: "Magic Pattern",
-        description: "Css gradient generator",
-        url: "https://www.magicpattern.design/tools/gradient-generator",
-        category: "frontend-tools",
+        title: 'Magic Pattern',
+        description: 'Css gradient generator',
+        url: 'https://www.magicpattern.design/tools/gradient-generator',
+        category: 'frontend-tools',
     },
     {
         id: 28,
         image: softrLogo,
-        title: "Softr",
-        description: "A collection of free tools that you can use to create designs",
-        url: "https://www.softr.io/tools",
-        category: "frontend-tools",
+        title: 'Softr',
+        description: 'A collection of free tools that you can use to create designs',
+        url: 'https://www.softr.io/tools',
+        category: 'frontend-tools',
     },
     {
         id: 29,
         image: l5Logo,
-        title: "100L5",
-        description: "Free all-in-one toolbox",
-        url: "https://10015.io/",
-        category: "frontend-tools",
+        title: '100L5',
+        description: 'Free all-in-one toolbox',
+        url: 'https://10015.io/',
+        category: 'frontend-tools',
     },
     {
         id: 30,
         image: getwavesLogo,
-        title: "Get Waves",
-        description: "Make some waves!",
-        url: "https://getwaves.io/",
-        category: "frontend-tools",
+        title: 'Get Waves',
+        description: 'Make some waves!',
+        url: 'https://getwaves.io/',
+        category: 'frontend-tools',
     },
     {
         id: 31,
         image: blobmakerLogo,
-        title: "Blob Maker",
-        description: "Make organic svg shapes for your next design",
-        url: "https://www.blobmaker.app/",
-        category: "frontend-tools",
+        title: 'Blob Maker',
+        description: 'Make organic svg shapes for your next design',
+        url: 'https://www.blobmaker.app/',
+        category: 'frontend-tools',
     },
     {
         id: 32,
         image: haikeiLogo,
-        title: "Haikei",
-        description: "Haikei is a web app to generate stunning visual content",
-        url: "https://haikei.app/",
-        category: "frontend-tools",
+        title: 'Haikei',
+        description: 'Haikei is a web app to generate stunning visual content',
+        url: 'https://haikei.app/',
+        category: 'frontend-tools',
     },
     {
         id: 33,
         image: gsourcesLogo,
-        title: "Clippy",
-        description: "The clip-path property allows you to make complex shapes in css",
-        url: "https://bennettfeely.com/clippy/",
-        category: "frontend-tools",
+        title: 'Clippy',
+        description: 'The clip-path property allows you to make complex shapes in css',
+        url: 'https://bennettfeely.com/clippy/',
+        category: 'frontend-tools',
     },
     {
         id: 34,
         image: shecodesLogo,
-        title: "SheCodes",
-        description: "Everything you nedd to generate awesome css for your next project",
-        url: "https://generators.shecodes.io/",
-        category: "frontend-tools",
+        title: 'SheCodes',
+        description: 'Everything you nedd to generate awesome css for your next project',
+        url: 'https://generators.shecodes.io/',
+        category: 'frontend-tools',
     },
+    {
+        id: 35,
+        image: scrollrevealLogo,
+        title: 'ScrollReveal',
+        description: 'ScrollReveal is a javascript library for easily animating elements as they enter/leave the viewport',
+        url: 'https://scrollrevealjs.org/',
+        category: 'javascript-libraries'
+    },
+    {
+        id: 36,
+        image: gsourcesLogo,
+        title: 'Locomotive Scroll',
+        description: 'A simple scroll library used by developers at locomative',
+        url: 'https://locomotivemtl.github.io/locomotive-scroll/',
+        category: 'javascript-libraries'
+    },
+    {
+        id: 37,
+        image: gsourcesLogo,
+        title: 'Slick',
+        description: 'The last carousel youll ever need',
+        url: 'https://scrollrevealjs.org/',
+        category: 'javascript-libraries'
+    },
+    {
+        id: 38,
+        image: emblacarouselLogo,
+        title: 'Embla Carousel',
+        description: 'A Lightweight Carousel Library With Fluid Motion And Great Swipe Precision',
+        url: 'https://www.embla-carousel.com/',
+        category: 'javascript-libraries'
+    }
 ];
