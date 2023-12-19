83a0bb9987402303a1effc76f6334fd9c94dee9a
gabrillojansen <Gabrillojansen@gmail.com>
2023-12-19 23:09:13 +0800

    commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const Footer = () => {
   return (
     <div className="w-full flex items-center justify-center pt-40 pb-8">
-        <p className="text-[--footer-text1] text-[13px]"><a className="text-[--footer-text]" href="https://www.facebook.com/gabrillojansen">&copy;gabrillojansen.</a> All rigths reserved</p>
+        <p className="text-[--footer-text1] text-[13px]"><a className="text-[--footer-text]" href="https://www.facebook.com/gabrillojansen">&copy; gabrillojansen.</a> All rigths reserved</p>
     </div>
   )
 }
