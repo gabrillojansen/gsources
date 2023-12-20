76443f3281d76f323ca47c7a9b2286e63b12e4ee
gabrillojansen <Gabrillojansen@gmail.com>
2023-12-20 15:57:38 +0800

    commit

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -5,16 +5,17 @@ import { Item } from './Item';
 export const Content = (props) => {
     const { ITEMS } = useContext(Context);
 
-    // Filter ITEMS based on category
-    const filteredItems = ITEMS.filter(ITEM => props.category === "/" || props.category === ITEM.category);
-
     return (
-        <div className="w-full flex items-center justify-center">
-            <div className="mt-16 max-w-[1290px] m-auto grid justify-center grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10">
-                {filteredItems.map((ITEM) => (
-                    <Item key={ITEM.id} itemData={ITEM} />
-                ))}
-            </div>
+      <div className="w-full flex items-center justify-center">
+        <div className="mt-16 max-w-[1290px] m-auto grid justify-center grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10">
+          {ITEMS.map((ITEM) => {
+            if (props.category === "/" || props.category === ITEM.category) {
+              return <Item key={ITEM.id} itemData={ITEM}/>;
+            } else {
+              return null;
+            }
+          })}
         </div>
+      </div>
     );
 };
